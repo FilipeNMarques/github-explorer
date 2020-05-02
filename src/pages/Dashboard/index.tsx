47a0cf40ba9f57f976aa6ae03e38fcae8ba58c0e
Filipe Marques <filipi.marques@gmail.com>
2020-05-02 20:34:22 -0300

    Add dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import geLogo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard Heeey</h1>;
+  return (
+    <>
+      <img src={geLogo} alt="Github explorer Logo" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
+            alt="Avatar Repository"
+          />
+          <div>
+            <strong>Gatsbyjs/gatsby</strong>
+            <p>Build blazing fast, modern apps and websites with React</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
+            alt="Avatar Repository"
+          />
+          <div>
+            <strong>Gatsbyjs/gatsby</strong>
+            <p>Build blazing fast, modern apps and websites with React</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
+            alt="Avatar Repository"
+          />
+          <div>
+            <strong>Gatsbyjs/gatsby</strong>
+            <p>Build blazing fast, modern apps and websites with React</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
+            alt="Avatar Repository"
+          />
+          <div>
+            <strong>Gatsbyjs/gatsby</strong>
+            <p>Build blazing fast, modern apps and websites with React</p>
+          </div>
+          <FiChevronRight size={25} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
