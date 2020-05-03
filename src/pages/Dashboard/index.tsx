5073eacaf6663be15e97ac5dfc9f2e4beb7bb53e
Filipe Marques <filipi.marques@gmail.com>
2020-05-03 18:44:28 -0300

    Call Github API, error handling and error input style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,68 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+
+// Styles
 import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories, Error } from './styles';
 import geLogo from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+// Services
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [inputError, setInputError] = useState('');
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite author/repository para adicionar na lista');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+      const repo = response.data;
+
+      setRepositories([...repositories, repo]);
+      setInputError('');
+      setNewRepo('');
+    } catch (err) {
+      setInputError('Erro ao buscar repositório');
+    }
+  }
+
   return (
     <>
       <img src={geLogo} alt="Github explorer Logo" />
       <Title>Explore repositórios no Github.</Title>
 
-      <Form>
-        <input placeholder="Nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="test">
-          <img
-            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
-            alt="Avatar Repository"
-          />
-          <div>
-            <strong>Gatsbyjs/gatsby</strong>
-            <p>Build blazing fast, modern apps and websites with React</p>
-          </div>
-          <FiChevronRight size={25} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
-            alt="Avatar Repository"
-          />
-          <div>
-            <strong>Gatsbyjs/gatsby</strong>
-            <p>Build blazing fast, modern apps and websites with React</p>
-          </div>
-          <FiChevronRight size={25} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
-            alt="Avatar Repository"
-          />
-          <div>
-            <strong>Gatsbyjs/gatsby</strong>
-            <p>Build blazing fast, modern apps and websites with React</p>
-          </div>
-          <FiChevronRight size={25} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://camo.githubusercontent.com/ac31ac54c2013850b0fb8a3a4926f4718a398fb3/68747470733a2f2f7777772e6761747362796a732e6f72672f6d6f6e6f6772616d2e737667"
-            alt="Avatar Repository"
-          />
-          <div>
-            <strong>Gatsbyjs/gatsby</strong>
-            <p>Build blazing fast, modern apps and websites with React</p>
-          </div>
-          <FiChevronRight size={25} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={25} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
